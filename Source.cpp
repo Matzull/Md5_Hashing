40d4484c4c421eb8f7c1a237093111d5df1a9a12
Matzull <marcal16@ucm.es>
2022-10-04 16:37:55 +0200

    Wip multithreading

--- Source.cpp
+++ Source.cpp
@@ -1,16 +1,22 @@
 #include <iostream> //for std::cout
 #include <string> //for std::string
-#include <string_view>
 #include <fstream>
 #include "MD5.h"
 #include <climits>
 #include <chrono>
+#include <thread>
+#include <mutex>
+#include <vector>
 using namespace std::chrono;
 using namespace std;
-
-string process(long long int& nonce);
-string faster(long long int& nonce);
+void fasterHashing(int thread_no, string& out, const string& guide);
 void initBlock();
+bool stopThreads(int thread_no);
+
+string guide;
+const char lf = 0x0A;
+const int threadcount = 1;
+vector<jthread> hashing_threads;
 
 int main(int argc, char* argv[])
 {
@@ -19,72 +25,24 @@ int main(int argc, char* argv[])
     cout << "Mining..." << endl;
     initBlock();
     auto start = high_resolution_clock::now();
-    for (long long int i = 0; i < LLONG_MAX && !found; i++)
-    {
-        hash = faster(i);
-        if (hash.substr(0, 7) == "bbbbbbb")//Aqui se define la complejidad
-        {
-            auto stop = high_resolution_clock::now();
-            auto duration = duration_cast<milliseconds>(stop - start); 
-            cout <<"Block found in:" << duration.count()/1000.0<< " seconds." << endl;
-            cout << "Average hashing power was: " << i / (duration.count() / 1000.0) << " H/S" << endl;
 
-            found = true;
-            cout << hash << endl;
-        }
-        else if(i % 100000 == 0)
-        {
-            cout << hash << endl;
-        }
+    string out;
+
+    for (int i = 0; i < threadcount; i++)
+    {
+        hashing_threads.push_back(jthread(fasterHashing, i, ref(out), ref(guide)));
     }
-    
+    for (auto& t : hashing_threads)
+    {
+        t.join();
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Block found in:" << duration.count() / 1000.0 << " seconds." << endl;
+    //cout << "Average hashing power was: " << i / (duration.count() / 1000.0) << " H/S" << endl;
+    cout << out << endl;
     return 0;
 }
-
-
-
-void updateNonce(const long long int& nonce)
-{
-    ifstream infile("bloque2.txt");
-    string texto = "";
-    string texto2 = "";
-    string text = "";
-    getline(infile, texto);
-    getline(infile, texto2);
-    ofstream outfile("bloque1.txt", ios_base::binary);
-    char lf = 0x0A;
-    text += texto + lf + texto2 + lf + "Nonce: " + to_string(nonce) + lf;
-    outfile << text;
-    infile.close();
-    outfile.close();
-}
-
-string process(long long int& nonce)
-{
-    updateNonce(nonce);
-    //Start opening your file
-    std::ifstream inBigArrayfile;
-    inBigArrayfile.open("bloque1.txt", std::ios::binary | std::ios::in);
-
-    //Find length of file
-    inBigArrayfile.seekg(0, std::ios::end);
-    long Length = inBigArrayfile.tellg();
-    inBigArrayfile.seekg(0, std::ios::beg);
-
-    //read in the data from your file
-    char* InFileData = new char[Length];
-    inBigArrayfile.read(InFileData, Length);
-
-    //Calculate MD5 hash
-    string Temp = md5(InFileData, Length);
-
-    //Clean up
-    delete[] InFileData;
-    return Temp + " " + to_string(nonce);
-}
-
-string guide;
-char lf = 0x0A;
 
 void initBlock()
 {
@@ -97,16 +55,46 @@ void initBlock()
     infile.close();
 }
 
-string faster(long long int& nonce)
+void fasterHashing(int thread_no, string& out, const string& guide)
 {
-    string text = guide + to_string(nonce) + lf;
+    long long int relNonce = 0;
+    for (size_t nonce = thread_no; nonce < LLONG_MAX; nonce += threadcount)
+    {
+        string text = guide + to_string(nonce) + lf;
 
-    int len = text.length();
-    char char_array[1000];
+        int len = text.length();
+        char char_array[1000];
 
-    // copying the contents of the
-    // string to char array
-    strcpy_s(char_array, len + 1, text.c_str());
-    string Temp = md5(char_array, len);
-    return Temp + " " + to_string(nonce);
+        strcpy_s(char_array, len + 1, text.c_str());
+        string hash = md5(char_array, len);
+        if (hash.substr(0, 7) == "bbbbbbb")
+        {
+            //stop all running threads and return value
+            stopThreads(thread_no);
+            out = hash + " " + to_string(nonce);
+            return;
+        }
+        else if (relNonce % 100000 == 0)
+        {
+            cout << hash << " " + to_string(nonce) << " Thread: " << thread_no << endl;
+        }
+        relNonce++;
+    }
+    return;
+}
+
+bool stopThreads(int thread_no)
+{
+    try
+    {
+        for (size_t i = 0; i < hashing_threads.size() && i != thread_no; i++)
+        {
+            hashing_threads[i].request_stop();
+        }
+    }
+    catch (const std::exception&)
+    {
+        cout << "Couldn't stop threads" << endl;
+        return false;
+    }
 }
